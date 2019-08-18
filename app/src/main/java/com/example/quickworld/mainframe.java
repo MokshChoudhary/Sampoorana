d5484c99a364af4f909730fba25cc529910b16b7
MokshChoudhary <40104735+mokshchoudhary@users.noreply.github.com>
2019-08-19 03:14:08 +0530

    working of mainframe(sos) button

--- app/src/main/java/com/example/quickworld/mainframe.java
+++ app/src/main/java/com/example/quickworld/mainframe.java
@@ -2,13 +2,24 @@ package com.example.quickworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class mainframe extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainframe);
+        Button c = findViewById(R.id.call_sos);
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent call = new Intent(mainframe.this,bim.class);
+                startActivity(call);
+            }
+        });
     }
 }
